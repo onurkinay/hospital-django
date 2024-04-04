76d254fa13a51279ce3ac19e6bae457d6ddb598d
Onur Kınay <ben@onurkinay.com.tr>
2024-04-04 16:56:33 +0300

    added views

--- hospital/home/views.py
+++ hospital/home/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def home(request):
-    #return HttpResponse('Hello world!')
+def home(request): 
+    return render(request,'home/index.html')
+
+def about(request):
+    return render(request,'home/index.html')
+
+def contact(request):
     return render(request,'home/index.html')
