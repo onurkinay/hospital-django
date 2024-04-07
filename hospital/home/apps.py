2d98ca2dd5fc4e4ed657257c479bce65deb567b2
Onur Kınay <ben@onurkinay.com.tr>
2024-04-07 11:31:53 +0300

    created groups(roles)

--- hospital/home/apps.py
+++ hospital/home/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
 
-
 class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
+    def ready(self): 
+        from django.contrib.auth.models import Group
+        
+        Group.objects.get_or_create(name='Admin')
+        Group.objects.get_or_create(name='Patient')
+        Group.objects.get_or_create(name='Doctor')
+        Group.objects.get_or_create(name='Accountant')
+      
