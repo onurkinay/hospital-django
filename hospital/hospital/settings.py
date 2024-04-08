d94e796739f220f25d3b93a2961300accf7b889c
Onur Kınay <ben@onurkinay.com.tr>
2024-04-09 00:00:10 +0300

    reverse commit

--- hospital/hospital/settings.py
+++ hospital/hospital/settings.py
@@ -126,10 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = os.environ.get("DJANGO_STATIC_URL", "/static/")
-STATIC_ROOT = os.environ.get("DJANGO_STATIC_ROOT", "./static/")   
+STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [os.path.join(FRONTEND_DIR, 'build', 'static')]
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
