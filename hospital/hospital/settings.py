9f4a83a532390aa8b23f897eff0a92f1cf887325
Onur Kınay <ben@onurkinay.com.tr>
2024-04-08 23:31:19 +0300

    allowed hosts

--- hospital/hospital/settings.py
+++ hospital/hospital/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*y7!vkzgsvld@d@$xx1df&yoye8*((9le+6p4yigbzey##x5uu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 
 
 # Application definition
