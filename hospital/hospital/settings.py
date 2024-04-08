37894746af75e08ab640d175f97f909d946806b9
Onur Kınay <ben@onurkinay.com.tr>
2024-04-08 23:54:10 +0300

    changed static folder

--- hospital/hospital/settings.py
+++ hospital/hospital/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_URL = os.environ.get("DJANGO_STATIC_URL", "/static/")
 STATIC_ROOT = os.environ.get("DJANGO_STATIC_ROOT", "./static/")   
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [os.path.join(FRONTEND_DIR, 'build', 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
